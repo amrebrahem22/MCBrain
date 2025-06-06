ffabbddb819c410780fee686fec1610e97b5f32e
unknown <amrebrahem226@gmail.com>
2025-06-06 15:36:22 +0300

    hero section

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener("mousemove", (event) => {
+  const x = event.clientX / window.innerWidth - 0.5;
+  const y = event.clientY / window.innerHeight - 0.5;
+
+  document.querySelectorAll(".parallax").forEach((element) => {
+    const speed = element.getAttribute("data-speed");
+    element.style.transform = `translate(${x * speed * 20}px, ${
+      y * speed * 20
+    }px)`;
+  });
+});
